bb3d451bdffc6b2b7fb0d36a00eff15cda79e9ca
Riki <riki.jarvinen@gmail.com>
2024-01-23 10:48:04 +0200

    toka

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import { AppBar, Button, Toolbar } from '@mui/material';
 import './App.css';
+import TypeBox from './typebox/typeBox.js';
 
 function App() {
   return (
-    <div className="App">
-      yes
+    <div className="Main">
+      <AppBar position="static" sx={{ 
+        backgroundColor: "black", 
+      }}>
+        <Toolbar>
+          <Button variant="contained" sx={{
+            backgroundColor: "white",
+            color: "black",
+            left: "0%",
+          }}>Kirjaudu sisään
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <div className="App">
+        <TypeBox />
+      </div>
     </div>
   );
 }
---
+++ src/typebox/typeBox.js
@@ -0,0 +1,11 @@
+import './typeBox.css';
+
+function TypeBox() {
+    return (
+        <div className="typeBox">
+        
+        </div>
+    );
+}
+
+export default TypeBox;
